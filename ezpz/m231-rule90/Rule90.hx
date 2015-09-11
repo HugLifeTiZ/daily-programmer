dc215c58511aaf10ae31caac4c776a9f73a6879a
Trent McPheron <twilightinzero@gmail.com>
2015-09-11 15:05:43 -0400

    Use ints instead of bools.

--- ezpz/m231-rule90/Rule90.hx
+++ ezpz/m231-rule90/Rule90.hx
@@ -2,13 +2,13 @@ using Lambda;
 
 class Rule90 {
     static function main () {
-        var line = Sys.args()[0].split("").map(function(i) {return i == "1";});
+        var line = Sys.args()[0].split("").map(Std.parseInt);
         for (_ in 0...25) {
-            Sys.println(line.map(function(i) {return i ? "x" : " ";}).join(""));
+            Sys.println([for (item in line) item > 0 ? "x" : " "].join(""));
             var i = 0;
             line = line.map(function (_) {
-                return (i == 0 ? false : line[i - 1]) !=
-                 (i == line.length - 1 ? false : line[++i]);
+                return (i == 0 ? 0 : line[i - 1]) !=
+                 (i == line.length - 1 ? 0 : line[++i]) ? 1 : 0;
             });
         }
     }
