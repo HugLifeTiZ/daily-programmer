2eefe0da813e44c0f1c19f05ff1c5dadc1b9b969
Trent McPheron <twilightinzero@gmail.com>
2015-09-14 13:59:58 -0400

    Rewrote m232 to open a file.

--- ezpz/m232-palindrome/Palindrome.hx
+++ ezpz/m232-palindrome/Palindrome.hx
@@ -1,13 +1,25 @@
+import haxe.io.*;
+import sys.FileSystem;
+import sys.io.File;
 using Lambda;
+
 class Palindrome {
     static function main () {
-        // We don't need a number of lines but it's in the spec
-        // for the challenge so we just toss it.
-        var string = Sys.args().slice(1).join("").toLowerCase().split("")
-         .filter(function (item) { return ~/[a-z]/i.match(item); }).join("");
-        var reverse = string.split("").copy();
-        reverse.reverse();  // Haxe's reverse modifies the array in place.
-        Sys.println(string == reverse.join("") ?
-         "Palindrome" : "Not a palindrome");
+        var file = File.read(Sys.args()[0]);
+        
+        var array = [];
+        try {
+            while (true) array.push(file.readLine());
+        } catch (e:Eof) { }
+        
+        var str = array.join("").toLowerCase().split("").filter(
+         function (item) { return ~/[a-z]/i.match(item); }).join("");
+        if (str == "") {
+            Sys.println("Empty input!");
+            Sys.exit(1);
+        }
+        var rev = str.split("").copy();
+        rev.reverse();  // Haxe's reverse modifies the array in place.
+        Sys.println(str == rev.join("") ? "Palindrome" : "Not a palindrome");
     }
 }
