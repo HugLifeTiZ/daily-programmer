1ac475923780a68d90785958d5a6869b9c3a92ac
Trent McPheron <twilightinzero@gmail.com>
2015-10-12 08:54:04 -0400

    Refactor a little to shorten it up.

--- ezpz/m236-randombag/RandomBag.hx
+++ ezpz/m236-randombag/RandomBag.hx
@@ -7,19 +7,19 @@ class RandomBag {
             max = Sys.args()[0].toInt();
         if (Sys.args().length == 0 || Sys.args()[0].canParse()) {
             for (i in 0 ... max) {
-                if (bag.length == 0)
-                    bag = ["O", "I", "S", "Z", "L", "J", "T"].shuffle();
+                refillBag();
                 Sys.print(bag.pop() + (i == max - 1 ? "\n" : ""));
             }
         } else {
             for (seq in Sys.args()) {
                 var valid = seq.toArray().reduce(function (valid, char) {
-                    if (bag.length == 0)
-                        bag = ["O", "I", "S", "Z", "L", "J", "T"];
+                    refillBag();
                     return valid && bag.remove(char);
                 }, true);
                 Sys.println(seq + (valid ? " VALID" : " INVALID"));
             }
         }
     }
+    static function refillBag ()
+        if (bag.length == 0) bag = ["O","I","S","Z","L","J","T"].shuffle();
 }
