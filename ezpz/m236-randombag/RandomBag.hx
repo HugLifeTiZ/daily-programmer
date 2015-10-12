22d536b3e1cc0a89d1f354dcbaf5a26468966b3f
Trent McPheron <twilightinzero@gmail.com>
2015-10-12 08:37:28 -0400

    Complete m236's extra.

--- ezpz/m236-randombag/RandomBag.hx
+++ ezpz/m236-randombag/RandomBag.hx
@@ -1,11 +1,23 @@
 using Thx;
 class RandomBag {
+    static var bag = [];
     static function main () {
-        var bag = [];
-        for (i in 0 ... 50) {
-            if (bag.length == 0)
-                bag = ["O", "I", "S", "Z", "L", "J", "T"].shuffle();
-            Sys.print(bag.pop() + (i == 49 ? "\n" : ""));
+        switch (Sys.args().length) {
+        case 0:
+            for (i in 0 ... 50) {
+                if (bag.length == 0)
+                    bag = ["O", "I", "S", "Z", "L", "J", "T"].shuffle();
+                Sys.print(bag.pop() + (i == 49 ? "\n" : ""));
+            }
+        default:
+            for (seq in Sys.args()) {
+                var valid = seq.toArray().reduce(function (valid, char) {
+                    if (bag.length == 0)
+                        bag = ["O", "I", "S", "Z", "L", "J", "T"];
+                    return valid && bag.remove(char);
+                }, true);
+                Sys.println(seq + (valid ? " VALID" : " INVALID"));
+            }
         }
     }
 }
