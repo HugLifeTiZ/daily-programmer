72c0cfba9461fcfc626279fa704b0db8453853ec
Trent McPheron <twilightinzero@gmail.com>
2015-10-22 09:54:30 -0400

    Add /u/aaargha's input and slightly retool algorithm to solve it correctly.

--- medmeh/w237-heightmap/HeightMap.hx
+++ medmeh/w237-heightmap/HeightMap.hx
@@ -3,21 +3,21 @@ using Thx;
 typedef Box = { x1: Int, y1: Int, x2: Int, y2: Int, layer: Int };
 
 class HeightMap {
-    static var layers = ["#", "=", "-", "."];
+    static var layers = [" ", "#", "=", "-", "."];
     static var map: Array<Array<String>>;
     
     static function main () {
         map = sys.io.File.getContent(Sys.args()[0]).toLines()
          .filter.fn(~/^[\|\+\- ]+$/.match(_)).map.fn(_.toArray());
-        paintBox(0, 0, map[0].length - 1, map.length - 1, 0);
+        paintBox(0, 0, map[0].length, map.length, 0);
         for (line in map)
             Sys.println(line.join(""));
     }
     
     static function paintBox (x1, y1, x2, y2, layer) {
         var boxes: Array<Box> = [];
-        for (y in (y1 + 1) ... y2)
-            for (x in (x1 + 1) ... x2)
+        for (y in y1 ... y2)
+            for (x in x1 ... x2)
                 if (!boxes.any.fn(inBox(x, y, _)))
                     switch (map[y][x]) {
                     case "+":
@@ -27,10 +27,10 @@ class HeightMap {
                         boxes.push({x1: x, y1: y, x2: tx, y2: ty,
                          layer: layer + 1});
                     case " ":
-                        map[y][x] = layer < 4 ? layers[layer] : " ";
+                        map[y][x] = layer < 5 ? layers[layer] : " ";
                     }
         for (box in boxes)
-            paintBox(box.x1, box.y1, box.x2, box.y2, box.layer);
+            paintBox(box.x1 + 1, box.y1 + 1, box.x2, box.y2, box.layer);
     }
     
     inline static function inBox (x, y, box)
