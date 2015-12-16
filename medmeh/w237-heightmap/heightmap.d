c3789cb6d531be7803b948593df9c84aa58c339b
Trent McPheron <twilightinzero@gmail.com>
2015-12-16 10:51:05 -0500

    Remove global state.

--- medmeh/w237-heightmap/heightmap.d
+++ medmeh/w237-heightmap/heightmap.d
@@ -1,37 +1,36 @@
 import std.stdio, std.algorithm, std.array, std.regex;
 
-struct Box { ulong x1, y1, x2, y2, layer; }
-
-auto layers = [' ', '#', '=', '-', '.'];
-char[][] grid;
+struct Box {
+    ulong x1, y1, x2, y2, layer;
+    bool contains (ulong x, ulong y) {
+        return x1 <= x && x <= x2 && y1 <= y && y <= y2;
+    }
+}
 
 void main (string[] args) {
-    grid = stdin.byLine.map!"a.idup"
+    auto grid = stdin.byLine.map!"a.idup"
      .filter!(a => a.matchFirst(ctRegex!(`^[\|\+\- ]+$`))).map!"a.dup".array;
-    paintBox(0, 0, grid[0].length, grid.length, 0);
+    grid.paintBox(Box(0, 0, grid[0].length, grid.length, 0));
     foreach (char[] line; grid) writeln(line);
 }
 
-void paintBox (ulong x1, ulong y1, ulong x2, ulong y2, ulong layer) {
+void paintBox (char[][] grid, Box box) {
     Box[] boxes = [];
-    for (auto y = y1; y < y2; ++y) {
-        for (auto x = x1; x < x2; ++x) {
-            if (!boxes.any!(a => a.inBox(x, y))) {
-                final switch (grid[y][x]) {
+    for (auto y = box.y1; y < box.y2; ++y) {
+        for (auto x = box.x1; x < box.x2; ++x) {
+            if (!boxes.any!(a => a.contains(x, y))) {
+                switch (grid[y][x]) {
                 case '+':
                     auto tx = x + 1, ty = y + 1;
                     while (grid[y][tx] != '+') ++tx;
                     while (grid[ty][x] != '+') ++ty;
-                    boxes ~= Box(x, y, tx, ty, layer + 1);
+                    boxes ~= Box(x, y, tx, ty, box.layer + 1);
                     break;
                 case ' ':
-                    grid[y][x] = layer < 5 ? layers[layer] : ' ';
+                    grid[y][x] = box.layer < 5 ? " #=-.".dup[box.layer] : ' ';
                     break;
+                default: break;
     }   }   }   }
-    foreach (Box box; boxes)
-        paintBox(box.x1 + 1, box.y1 + 1, box.x2, box.y2, box.layer);
-}
-
-pure bool inBox (Box box, ulong x, ulong y) {
-    return box.x1 <= x && x <= box.x2 && box.y1 <= y && y <= box.y2;
+    foreach (Box b; boxes)
+        grid.paintBox(Box(b.x1 + 1, b.y1 + 1, b.x2, b.y2, b.layer));
 }
