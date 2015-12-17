92ced0afda4fc13cea7b91568473732db79b8b02
Trent McPheron <twilightinzero@gmail.com>
2015-12-17 11:12:50 -0500

    Implement w236 in D.

---
+++ medmeh/w236-fibonaccish/fibonaccish.d
@@ -0,0 +1,16 @@
+import std.stdio, std.conv, std.algorithm;
+
+void main (string[] args) {
+    auto target = args[1].to!int;
+    for (auto i = 1; i < target; ++i) {
+        auto seq = [0, i];
+        while (seq[$ - 1] < target) {
+            seq ~= seq[$ - 1] + seq[$ - 2];
+        }
+        if (seq[$ - 1] == target) {
+            writeln("Target reached with f(1) = ", i, ".");
+            writeln(seq.map!(to!string).reduce!"a ~ \" \" ~ b");
+            break;
+        }
+    }
+}
