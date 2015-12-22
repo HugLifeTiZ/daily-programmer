976e0476546654554f7c2d8e0cd8e32853d045f6
Trent McPheron <twilightinzero@gmail.com>
2015-12-22 15:09:24 -0500

    Port m171 to D.

---
+++ ezpz/m171-hexbitmap/hexbitmap.d
@@ -0,0 +1,10 @@
+import std.stdio, std.algorithm, std.conv;
+
+void main (string[] args) {
+    args[1..$].map!(
+        a => ('1' ~ a)
+         .to!int(16)
+         .to!string(2)
+         .map!"a == '1' ? '█' : '░'"
+     ).each!writeln;
+}
