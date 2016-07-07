57f311c6f2f8697242ed2062cd004ecb529fbda1
Trent McPheron <twilightinzero@gmail.com>
2016-07-06 23:59:29 -0400

    Make the comments a little more correct.

--- ezpz/m274-beale/beale.d
+++ ezpz/m274-beale/beale.d
@@ -6,11 +6,11 @@ void main (string[] args) {
     const string letters = readText("doi.txt")
      .split  // Split each word by whitespace.
      .map!(a => a[0])  // Discard the rest of each word.
-     .array;  // Convert range to an array. D ranges are weird.
+     .array;  // Force evaluation of the range.
     const wrap = letters.length;
     stdin.byLineCopy.join(" ")  // Join each line into a single string.
      .filter!(a => a.inPattern("0-9 "))  // Filter out commas.
-     .array.split  // Convert the result to an array and split it by whitespace.
+     .array.split  // Force evaluation and split it by whitespace.
      .map!(a => letters[a.to!int - 1 % wrap]) // Look up letter and wrap around.
      .writeln;  // Write to stdout.
 }
