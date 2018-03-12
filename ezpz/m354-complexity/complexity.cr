40b4a3d1b75b1f602837708e595662c7498aec5a
Trent McPheron <twilightinzero@gmail.com>
2018-03-12 13:05:17 -0400

    Even zero is a truthy value so I can shortcut.

--- ezpz/m354-complexity/complexity.cr
+++ ezpz/m354-complexity/complexity.cr
@@ -1,5 +1,5 @@
 ARGV.each do |arg| 
-    if ! (target = arg.to_u64?).nil?
+    if target = arg.to_u64?
         smallest = UInt64::MAX
         (1..Math.sqrt(target).ceil).each do |i|
             if target % i == 0
